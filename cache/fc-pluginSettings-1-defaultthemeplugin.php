798a050b7c71064eadff7b9e1f491b09cb63ffe5
Cristiano Moura <crisbinhu456@gmail.com>
2023-09-05 19:49:15 -0300

    Initial Commit

---
+++ cache/fc-pluginSettings-1-defaultthemeplugin.php
@@ -0,0 +1,8 @@
+<?php return array (
+  'baseColour' => '#FFFFFF',
+  'displayStats' => 'none',
+  'enabled' => true,
+  'showDescriptionInJournalIndex' => 'true',
+  'typography' => 'notoSans',
+  'useHomepageImageAsHeader' => 'true',
+);
